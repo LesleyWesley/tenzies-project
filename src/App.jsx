64b89d5332406b2a21bee79f7b39667d432902f3
lesley_w <lrwesley92@gmail.com>
2024-01-13 16:32:42 -0600

    Built die component and styled

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,23 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Dice from "./components/Dice.jsx"
 
 function App() {
     return (
       <main>
-        <h1>This is the app!</h1>
+        <div className="dice-container">
+          <Dice number="6"/>
+          <Dice number="2" />
+          <Dice number="1" />
+          <Dice number="5" />
+          <Dice number="4" />
+          <Dice number="6"/>
+          <Dice number="2" />
+          <Dice number="1" />
+          <Dice number="5" />
+          <Dice number="4" />
+        </div>
       </main>
     )
 }
---
+++ src/components/Dice.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+function Dice(props) {
+  return (
+    <div className="die">{props.number}</div>
+  )
+}
+
+export default Dice
