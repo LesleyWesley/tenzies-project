5a0ea1a4b227b5f5475f5be4fabfd74e3987223c
lesley_w <lrwesley92@gmail.com>
2024-01-14 23:12:58 -0600

    Added nanoid as a dependency so all dice have a key prop

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { nanoid } from "nanoid"
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -9,10 +10,16 @@ function App() {
     function allNewDice() {
       let newDiceArray = []
       for (let i = 0; i < 10; i++) {
-        newDiceArray.push( Math.floor(Math.random() * 7))
+        newDiceArray.push({
+          value: Math.floor(Math.random() * 7),
+          isHeld: false,
+          id: nanoid()
+        })
       }
       return newDiceArray
     }
+
+    console.log(allNewDice())
 
     const [newDice, setNewDice] = React.useState(allNewDice())
 
@@ -22,7 +29,7 @@ function App() {
 
     const diceElements = newDice.map(die => {
       return (
-        <Dice  number={die}/>
+        <Dice  key={die.id} number={die.value}/>
       )
     })
 
